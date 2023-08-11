5fb927800895e263674279570af24ec23b2017f2
David Kamau Muchiri <david.muchiri@sibasi.ltd>
2023-08-11 12:38:37 +0300

    Updating landing message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello! The deployment script is working.';
+    return 'Hello! This is the new update. V1.0.1';
   }
 }
