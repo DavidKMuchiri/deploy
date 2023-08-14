c1878f87b311769d51626b205001691ff58d424c
David Kamau Muchiri <david.muchiri@sibasi.ltd>
2023-08-14 11:53:37 +0300

    Changing API version

--- API/src/app.service.ts
+++ API/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello! This is the new update. V1.0.1';
+    return 'Hello! This is the new update. V2.1.6';
   }
 }
